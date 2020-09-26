b30bc39398b934c8c64c516431430f12ff256925
Jalen Lewis <jalenlew1995@gmail.com>
2020-09-26 16:05:44 -0500

    Initial Commit

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+USE employeeTracker_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT(10)
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT(10),
+    manager_id INT(10) NULL,
+    PRIMARY KEY(id)
+);
