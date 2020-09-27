f155d0b4c0beabc4105b276b878348efaf588495
Jalen Lewis <jalenlew1995@gmail.com>
2020-09-26 20:11:36 -0500

    Edited schema.sql and js file

--- employee_tracker.js
+++ employee_tracker.js
@@ -1,1 +1,93 @@
 const mysql = require('mysql');
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Duke2017###",
+    database: "employeeTracker_db"
+});
+  
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
+
+function questions() {
+    inquirer.prompt({
+        message: "What action whould you like to take?",
+        type: "list",
+        choices: [
+            "View All Employees",
+            "View All Departments",
+            "Add Employee",
+            "Add Role",
+            "Add Department",
+            "Update Employee Role",
+            "Quit"
+        ],
+        name: "choice"
+    }).then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice) {
+            case "View All Employees":
+                viewAllEmployees()
+                break;
+              
+            case "View All Departments":
+                viewAllDepartments()
+                break; 
+              
+            case "Add Employee":
+                addEmployee()
+                break;  
+                  
+            case "Add Role":
+                addRole()
+                break;
+                  
+            case "Add Department":
+                addDepartment()
+                break;   
+                  
+            case "Update Employee Role":
+                updateEmployeeRole()
+                break;
+                  
+            default:
+                connection.end()
+                break;    
+        }
+    })
+}
+
+function viewAllEmployees() {
+
+}
+
+function viewAllDepartments() {
+
+}
+
+function addEmployee() {
+
+}
+
+function addRole() {
+
+}
+
+function addDepartment() {
+
+}
+
+function updateEmployeeRole() {
+
+}
--- schema.sql
+++ schema.sql
@@ -5,7 +5,6 @@ USE employeeTracker_db;
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30),
-    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
@@ -13,7 +12,6 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL(10,2),
     department_id INT(10)
-    PRIMARY KEY(id)
 );
 
 CREATE TABLE employee (
@@ -22,5 +20,4 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT(10),
     manager_id INT(10) NULL,
-    PRIMARY KEY(id)
 );
